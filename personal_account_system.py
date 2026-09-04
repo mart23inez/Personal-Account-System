4b81c26c1205007b1e2d62bfc30e69b713e79365
mart23inez <268530911+mart23inez@users.noreply.github.com>
2026-09-04 05:02:53 -0400

    Removed outdated files.

--- personal_account_system.py
+++ personal_account_system.py
@@ -35,66 +35,73 @@ else:
     username = ""
     password = ""
     passwordverify = ""
-    usermin = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ1234567890!@#$%^&*()"
+    numbers = "1234567890"
+    letters = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ"
+    specialchar = "!@#$%^&*()"
 
     while True:
+            
+            username = input("Please enter a username (3 - 64 characters): ")
+            if len(username) < 3:
+                print("Error: Username must be at least 3 characters, please try again.")
+            elif len(username) > 64:
+                print("Error: Username must be no more than 64 characters, please try again.")
+            else:
+                break
 
-        username = input("Please enter a username (Must be between 8 - 16 characters and include at least 1 number or special character 0-9): ")
+    while True:
+        password = input("Please enter a password (Must be at least 12 characters, include 1 letter, and include 1 number and special character (0-9): ")
         securedUP = False
-        index = 0
+        num_verification = False
+        letter_verification = False
+        specialchar_verification = False
         verification = False
-
-        if len(username) < 8:
-            print("Error: Username must be at least 8 characters, please try again.")
-        elif len(username) > 16:
-            print("Error: Username must be 16 characters or fewer, please try again.")
+        num_index = 0
+        let_index = 0
+        specchar_index = 0
+        if len(password) < 12:
+            print("Error: Password must be at least 12 characters, please try again.")
         else:
             verification = True
 
         if verification:
-            while index < len(usermin):
-                verify = usermin[index]
-                if verify in username:
-                    securedUP = True
+            while num_index < len(numbers):
+                verify = numbers[num_index]
+                if verify in password:
+                    num_verification = True
                     break
                 else:
-                    index += 1
+                    num_index += 1
             else:
-                print("Error: Username must include 1 number or special character, please try again.")
+                print("Error: Password must include 1 number, please try again.")
+
+        if num_verification:
+            while let_index < len(letters):
+                verify = letters[let_index]
+                if verify in password:
+                    letter_verification = True
+                    break
+                else:
+                    let_index += 1
+            else:
+                print("Error: Password must include 1 letter, please try again.")
+
+        if letter_verification:
+            while specchar_index < len(specialchar):
+                verify = specialchar[specchar_index]
+                if verify in password:
+                    specialchar_verification = True
+                    break
+                else:
+                    specchar_index += 1
+            else:
+                print("Error: Password must include 1 special character, please try again.")
+
+            if num_verification and specialchar_verification and letter_verification:
+                securedUP = True
+
         if securedUP:
             break
-
-    while True:
-
-        password = input("Please enter a password (Must be between 8 - 16 characters and include at least 1 number or special character 0-9): ")
-        securedUP = False
-        index = 0
-        verification = False
-
-        if len(password) < 8:
-            print("Error: Password must be at least 8 characters, please try again.")
-        elif len(password) > 16:
-            print("Error: Password must be 16 characters or fewer, please try again.")
-        else:
-            verification = True
-
-        if verification:
-            while index < len(usermin):
-                verify = usermin[index]
-                if verify in password:
-                    securedUP = True
-                    break
-                else:
-                    index += 1
-            else:
-                print("Error: Password must include 1 number or special character, please try again.")
-        if securedUP:
-            passwordverify = input("Please enter the password again: ")
-            if passwordverify == password:
-                print("Account successfully created.")
-                break
-            else:
-                print("Error: Passwords must match, please try again.")
 
 print(f"Welcome {username}!")
 
